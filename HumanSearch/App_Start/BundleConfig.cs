052b7892a5ba396c5bb9d0c4aafe4eb8d5a3ba25
airburple <airburple@hotmail.com>
2016-09-02 16:18:28 -0600

    Added controllerjs modulejs and servicejs files and updated the shared layout

--- HumanSearch/App_Start/BundleConfig.cs
+++ HumanSearch/App_Start/BundleConfig.cs
@@ -8,6 +8,16 @@ namespace HumanSearch
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
+            bundles.Add(new ScriptBundle("~/bundles/angularJS").Include(
+                     "~/Scripts/angular.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/customJS").Include(
+                                 "~/Scripts/HumanScripts/Module.js",
+                                 "~/Scripts/HumanScripts/Service.js",
+                                 "~/Scripts/HumanScripts/Controller.js"));
+
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
